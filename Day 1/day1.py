c9d9b05247655546d6d30a9f38f3f993073384d7
Richard King <richard.king@bjss.com>
2022-12-01 20:36:54 +0000

    Tried to refactor loop into list comprehension; syntax error

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,19 +1,20 @@
+# Challenge details: https://adventofcode.com/2022/day/1
+
 with open('input.txt') as f:
     input = f.readlines()
 
 list = [i.rstrip() for i in input]
 
-totals = []
+calorie_totals = []
 elf_calories = 0
 
-for l in list:
-    if l != '':
-        elf_calories += int(l)
-    else:
-        totals.append(elf_calories)
-        elf_calories = 0
+def store(elf_calories):
+    calorie_totals.append(elf_calories)
+    elf_calories = 0
 
-totals.sort(reverse=True)
+[ elf_calories += int(calories) if calories != '' else store(elf_calories) for calories in list ]
 
-print("Part 1:", max(totals))
-print("Part 2:", sum(totals[:3]))
+calorie_totals.sort(reverse=True)
+
+print("Part 1:", max(calorie_totals))
+print("Part 2:", sum(calorie_totals[:3]))
