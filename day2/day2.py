758dcd2ba0041d0e76f97db1c5b3242dea34394f
Richard King <richard.king@bjss.com>
2022-12-07 21:30:58 +0000

    Solution to day 2 part 2 (horrible nested if statements)

--- day2/day2.py
+++ day2/day2.py
@@ -4,9 +4,9 @@ with open('input.txt') as f:
 cleanedInput = [i.rstrip() for i in input]
 games = [tuple(map(str, sub.split(' '))) for sub in cleanedInput]
 
-winConditions = [("A", "Y"), ("B", "Z"), ("C", "X")]
+winConditions  = [("A", "Y"), ("B", "Z"), ("C", "X")]
 drawConditions = [("A", "X"), ("B", "Y"), ("C", "Z")]
-loseConditions = [("A","Z"), ("B", "X"), ("C", "Y")]
+loseConditions = [("A", "Z"), ("B", "X"), ("C", "Y")]
 score = 0
 
 def getShapeScore(shape):
@@ -27,7 +27,42 @@ def getOutcomeScore(game):
     elif game in loseConditions:
         return 0
     else:
-        raise Exception("Input Error")
+        raise Exception("Input error")
+
+rock = 1
+paper = 2
+scissors = 3
+lose = 0
+draw = 3
+win = 6
+
+def getScore(game):
+    if game[0] == "A":
+        if game[1] == "X":
+            return scissors + lose
+        elif game[1] == "Y":
+            return rock + draw
+        elif game[1] == "Z":
+            return paper + win
+
+    elif game[0] == "B":
+        if game[1] == "X":
+            return rock + lose
+        elif game[1] == "Y":
+            return paper + draw
+        elif game[1] == "Z":
+            return scissors + win
+
+    elif game[0] == "C":
+        if game[1] == "X":
+            return paper + lose
+        elif game[1] == "Y":
+            return scissors + draw
+        elif game[1] == "Z":
+            return rock + win
+
+    else:
+        raise Exception("Input error")
 
 for game in games:
     score += getShapeScore(game[1])
@@ -35,3 +70,9 @@ for game in games:
 
 print("Part 1: ", score)
 
+score = 0
+
+for game in games:
+    score += getScore(game)
+
+print("Part 2: ", score)
