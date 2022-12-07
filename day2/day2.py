0f2b5b49f7e760e4d2c7ecfe39cd186da6787d01
Richard King <richard.king@bjss.com>
2022-12-07 21:32:33 +0000

    Small refactor

--- day2/day2.py
+++ day2/day2.py
@@ -7,27 +7,8 @@ games = [tuple(map(str, sub.split(' '))) for sub in cleanedInput]
 winConditions  = [("A", "Y"), ("B", "Z"), ("C", "X")]
 drawConditions = [("A", "X"), ("B", "Y"), ("C", "Z")]
 loseConditions = [("A", "Z"), ("B", "X"), ("C", "Y")]
+
 score = 0
-
-def getShapeScore(shape):
-    if shape == "X":
-        return 1
-    elif shape == "Y":
-        return 2
-    elif shape == "Z":
-        return 3
-    else:
-        raise Exception("Input error")
-
-def getOutcomeScore(game):
-    if game in winConditions:
-        return 6
-    elif game in drawConditions:
-        return 3
-    elif game in loseConditions:
-        return 0
-    else:
-        raise Exception("Input error")
 
 rock = 1
 paper = 2
@@ -35,6 +16,26 @@ scissors = 3
 lose = 0
 draw = 3
 win = 6
+
+def getShapeScore(shape):
+    if shape == "X":
+        return rock
+    elif shape == "Y":
+        return paper
+    elif shape == "Z":
+        return scissors
+    else:
+        raise Exception("Input error")
+
+def getOutcomeScore(game):
+    if game in winConditions:
+        return win
+    elif game in drawConditions:
+        return draw
+    elif game in loseConditions:
+        return lose
+    else:
+        raise Exception("Input error")
 
 def getScore(game):
     if game[0] == "A":
